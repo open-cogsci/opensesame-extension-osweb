f6c536ddcc8112d7a726bf7e97577df18b255d42
dschreij <dschreij@gmail.com>
2018-09-04 18:00:01 +0200

    Further improvements to JATOS packaging

--- osweb/export.py
+++ osweb/export.py
@@ -37,140 +37,162 @@ tmpl_folder = os.path.join(os.path.dirname(__file__), u'src', u'html')
 
 
 def standalone(osexp, dst, subject=0, fullscreen=False):
-    params = {'subject': subject, 'fullscreen': fullscreen}
-    mode = u'standalone'
-    _html(osexp, dst, mode, _js_files(mode), params, bundled=True)
+	params = {'subject': subject, 'fullscreen': fullscreen}
+	mode = u'standalone'
+	_html(osexp, dst, mode, _js_files(mode), params, bundled=True)
 
 def jatos(
-    osexp,
-    dst,
-    title='My OpenSesame experiment',
-    description='No description',
-    subject=0,
-    fullscreen=False
+	osexp,
+	dst,
+	title='My OpenSesame experiment',
+	description='No description',
+	subject=0,
+	fullscreen=False
 ):
 
-    mode = u'jatos'
-    asset = _unique_hash()
-    dirname = tempfile.mkdtemp(suffix=u'.jatos')
-    os.mkdir(os.path.join(dirname, asset))
-    asset_path = os.path.join(dirname, asset, u'index.html')
-    jas_path = os.path.join(dirname, u'info.jas')
+	mode = u'jatos'
+	asset = _unique_hash()
+	dirname = tempfile.mkdtemp(suffix=u'.jatos')
+	os.mkdir(os.path.join(dirname, asset))
+	asset_path = os.path.join(dirname, asset, u'index.html')
+	jas_path = os.path.join(dirname, u'info.jas')
 
-    js_sources = _js_files(mode)
+	js_sources = _js_files(mode)
 
-    params = {'subject': subject, 'fullscreen': fullscreen}
+	params = {'subject': subject, 'fullscreen': fullscreen}
 
-    _html(
-        osexp,
-        asset_path,
-        mode,
-        js_sources,
-        params
-    )
+	_html(
+		osexp,
+		asset_path,
+		mode,
+		js_sources,
+		params
+	)
 
-    info = {
-    	'version': '3',
-    	'data': {
-    		'title': title,
-    		'description': description,
-    		'groupStudy': False,
-    		'dirName': asset,
-    		'comments': '',
-    		'jsonData': None,
-    		'componentList': [{
-    			'title': 'Experiment',
-    			'htmlFilePath': 'index.html',
-    			'reloadable': False,
-    			'active': True,
-    			'jsonData': None
-    		}],
-    		'batchList': []
-    	}
-    }
+	info = {
+		'version': '3',
+		'data': {
+			'title': title,
+			'description': description,
+			'groupStudy': False,
+			'dirName': asset,
+			'comments': '',
+			'jsonData': None,
+			'componentList': [{
+				'title': 'Experiment',
+				'htmlFilePath': 'index.html',
+				'reloadable': False,
+				'active': True,
+				'jsonData': None
+			}],
+			'batchList': []
+		}
+	}
 
-    with open(jas_path, u'w') as fd:
-        json.dump(info, fd)
+	with open(jas_path, u'w') as fd:
+		json.dump(info, fd)
 
-    with zipfile.ZipFile(dst, 'w') as fd:
-        fd.write(jas_path, u'info.jas')
-        fd.write(asset_path, os.path.join(asset, u'index.html'))
-        for js in js_sources:
-            fd.write(js['src'], os.path.join(asset, js['dest']))
+	with zipfile.ZipFile(dst, 'w') as fd:
+		fd.write(jas_path, u'info.jas')
+		fd.write(asset_path, os.path.join(asset, u'index.html'))
+		fd.write(osexp, os.path.join(asset, os.path.basename(osexp)))
+		for js in js_sources:
+			fd.write(js['src'], os.path.join(asset, js['dest']))
 
 
 # Private functions
 
 def _js_files(mode):
 
-    # The osweb source and vendor js bundles.
-    jsFiles = [
-        {'src': os.path.join(js_folder, basename), 'dest': os.path.join('js', basename)}
-        for basename in os.listdir(js_folder)
-        if basename.startswith(u'osweb') or basename.startswith(u'vendors~osweb.')
-    ]
+	# The osweb source and vendor js bundles.
+	jsFiles = [
+		{'src': os.path.join(js_folder, basename), 'dest': os.path.join(u'js', basename)}
+		for basename in os.listdir(js_folder)
+		if basename.startswith(u'osweb') or basename.startswith(u'vendors~osweb.')
+	]
 
-    #  The current environment js (Jatos or otherwise)
-    envJs = u'{}.js'.format(mode)
-    jsFiles.append({'src': os.path.join(js_folder, envJs), 'dest': os.path.join('js', envJs)})
-    return jsFiles
+	#  The current environment js (Jatos or otherwise)
+	envJs = u'{}.js'.format(mode)
+	jsFiles.append({'src': os.path.join(js_folder, envJs), 'dest': os.path.join(u'js', envJs)})
+	return jsFiles
+
 
 def _html(osexp, dst, type_, js=None, params=None, bundled=False):
-    # js = js_osweb + [os.path.join(js_folder, u'{}.js'.format(type_))]
-    js = js or []
-    tmpl = os.path.join(tmpl_folder, u'{}.html'.format(type_))
 
-    # The HTML file parsed as a DOM Tree
-    with open(tmpl, 'r') as t_pid:
-        dom = BeautifulSoup(t_pid, 'html.parser')
+	js = js or []
+	tmpl = os.path.join(tmpl_folder, u'{}.html'.format(type_))
 
-    if bundled:
-        scriptTag = dom.new_tag('script', type="text/javascript")
-        if params:
-            # Embed the params as JSON
-            scriptTag.append('const params = JSON.parse(\'{}\')'.format(json.dumps(params)))
-        for js_file in js:
-            scriptTag.append(_read(js_file))
-        dom.head.append(scriptTag)
-    else:
-        # If a json params file has been specified, save it as a node
-        if params:
-            # Create a script node that references the JSON file
-            scriptTag = dom.new_tag('script', id="parameters", type="text/javascript")
-            scriptTag.append('const params = JSON.parse(\'{}\')'.format(json.dumps(params)))
-            dom.head.append(scriptTag)
-        for js_file in js:
-            scriptTag = dom.new_tag('script', src=js_file['dest'], type="text/javascript")
-            dom.head.append(scriptTag)
+	# The HTML file parsed as a DOM Tree
+	with open(tmpl, 'r') as t_pid:
+		dom = BeautifulSoup(t_pid, u'html.parser')
 
-    expTag = dom.new_tag(
-        'embed',
-        id='osexp_src',
-        src='data:application/gzip;base64,' + _read_b64(osexp),
-        style='display:none'
-    )
-    dom.body.append(expTag)
+	if bundled:
+		_compose_for_standalone(osexp, dom, js, params)
+	else:
+		_compose_for_jatos(osexp, dom, js, params)
+		# If a json params file has been specified, save it as a node
 
-    with open(dst, 'w') as fd:
-        fd.write(dom.prettify())
+	with open(dst, 'w') as fd:
+		fd.write(dom.prettify())
+
+
+def _compose_for_standalone(osexp, dom, js, params=None):
+	""" Builds on top of the base HTML template to create a structure that is appropriate
+	for a standalone HTML file """
+
+	scriptTag = dom.new_tag(u'script', type=u"text/javascript")
+	if params:
+		# Embed the params as JSON
+		scriptTag.append(u'const params = JSON.parse(\'{}\')'.format(json.dumps(params)))
+	for js_file in js:
+		scriptTag.append(_read(js_file))
+	dom.head.append(scriptTag)
+
+	# Add experiment as base64 encoded string
+	expTag = dom.new_tag(
+		'embed',
+		id='osexp_src',
+		src='data:application/gzip;base64,' + _read_b64(osexp),
+		style='display:none'
+	)
+	dom.body.append(expTag)
+
+
+def _compose_for_jatos(osexp, dom, js, params=None):
+	""" Builds on top of the base HTML template to create a structure that is appropriate
+	for integration in JATOS """
+
+	scriptTag = dom.new_tag('script', id="parameters", type="text/javascript")
+	if params:
+		# Create a script node that exposes the experiment's parameters
+		scriptTag.append(u'const params = JSON.parse(\'{}\')'.format(json.dumps(params)))
+		dom.head.append(scriptTag)
+
+	# Get the OpenSesame experiment file name, and add a JS variable to its location
+	scriptTag.append(u'const osexpFile = "{}"'.format(os.path.basename(osexp)))
+
+	# Add script nodes referencing the sources of all other required javascript files.
+	for js_file in js:
+		scriptTag = dom.new_tag('script', src=js_file['dest'], type="text/javascript")
+		dom.head.append(scriptTag)
 
 
 def _read(path):
 
-    with open(path) as fd:
-        return fd.read()
+	with open(path) as fd:
+		return fd.read()
 
 
 def _read_b64(path):
 
-    with open(path, 'rb') as fd:
-        e = base64.b64encode(fd.read())
-        return e.decode()
+	with open(path, 'rb') as fd:
+		e = base64.b64encode(fd.read())
+		return e.decode()
 
 def _unique_hash():
 
-    return hashlib.md5(str(time.time()).encode()).hexdigest()
+	return hashlib.md5(str(time.time()).encode()).hexdigest()
 
 if __name__ == "__main__":
-    jatos('','output.zip')
+	jatos('','output.zip')
 
--- osweb/src/js/jatos.js
+++ osweb/src/js/jatos.js
@@ -1,52 +1,29 @@
 let context = {
-	debug: false,
-	subject: params.subject,
-	fullScreen: params.fullscreen,
-	introClick: false,
-	introScreen: true,
-	mimetype: '',
-	name: 'osweb',
-	prompt: null,
-	scaleMode: 'exactFit',
-	source: null,
-	target: null,
-	confirm: onConfirm,
-	onLog: onLogHandler,
-	onFinished: onFinishedHandler
-}
+    source: (new URL(osexpFile, window.location)).href,
+    debug: false,
+    subject: params.subject,
+    fullScreen: params.fullscreen,
+    introClick: false,
+    introScreen: true,
+    mimetype: '',
+    name: 'osweb',
+    prompt: null,
+    scaleMode: 'exactFit',
+    target: null,
+    confirm: onConfirm,
+    onLog: onLogHandler,
+    onFinished: onFinishedHandler
+};
 
-let taskData = ''
-let runner = null
-let errorsOccured = false
-let errorMessage = ''
-
-/**
- * Converts base-64-encoded data to a File object, which can be passed to
- * osweb as an experiment file
- **/
-function URItoFile(uri) {
-	let byteCharacters = atob(uri.split(',')[1])
-	let byteArrays = []
-	for (let offset = 0; offset < byteCharacters.length; offset += 512) {
-		let slice = byteCharacters.slice(offset, offset + 512);
-		let byteNumbers = new Array(slice.length)
-		for (let i = 0; i < slice.length; i++) {
-			byteNumbers[i] = slice.charCodeAt(i)
-		}
-		let byteArray = new Uint8Array(byteNumbers)
-		byteArrays.push(byteArray)
-	}
-	let blob = new Blob(byteArrays)
-	return new File([blob], "osexp_src")
-}
+let runner = null;
+let errorsOccured = false;
 
 /**
  * Is called on page load to launch the experiment
  */
 function runExperiment() {
-	context.source = URItoFile(document.getElementById('osexp_src').src)
-	runner = osweb.getRunner('osweb_div')
-	runner.run(context)
+    runner = osweb.getRunner('osweb_div');
+    runner.run(context);
 }
 
 /**
@@ -54,22 +31,21 @@ function runExperiment() {
  * @param {Object} data - The result data.
  */
 function onLogHandler(data) {
-	if (data === null) {
-		return
-	}
-	// Add Jatos parameters to this log entry
-	jatos.addJatosIds(data)
+    if (data === null) {
+        return;
+    }
+    // Add Jatos parameters to this log entry
+    jatos.addJatosIds(data);
 
-	// Send this log entry to the server
-	jatos.appendResultData(
-		JSON.stringify(data),
-		() => {},
-		() => {
-			// How to handle errors?
-			errorsOccured = true
-			errorMessage = 'Error occurred for ' + JSON.stringify(data)
-		}
-	)
+    // Send this log entry to the server
+    jatos.appendResultData(
+        JSON.stringify(data),
+        () => {},
+        () => {
+            // How to handle errors?
+            errorsOccured = true;
+        }
+    );
 }
 
 /** Callback function for processing after an experiment is finished.
@@ -77,18 +53,19 @@ function onLogHandler(data) {
  * @param {Object} sessionData - The session data.
  */
 function onFinishedHandler(data, sessionData) {
-	if (errorsOccured) {
-		jatos.endStudy(false, 'Error occurred while sending: ' + JSON.stringify(data))
-	} else {
-		jatos.endStudy('Study completed successfully')
-	}
-	document.getElementById("osweb_div").style.display = "none"
+    if (errorsOccured) {
+        jatos.endStudy(false, 'Error occurred while sending: ' + JSON.stringify(data));
+    } else {
+        jatos.endStudy('Study completed successfully');
+    }
+    document.getElementById('osweb_div').style.display = 'none';
 }
 
 /** Callback function for handling Escape key presses
  */
 function onConfirm() {
-	runner.exit()
+    jatos.endStudy(false, 'Experiment aborted');
+    runner.exit();
 }
 
-jatos.onLoad(runExperiment)
+jatos.onLoad(runExperiment);
