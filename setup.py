cd18251f8b29e13289043abf9502766d5d51ec05
Sebastiaan Mathot <s.mathot@cogsci.nl>
2018-10-02 20:52:22 +0200

    Bump to 1.1.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 	# Some general metadata. By convention, a extension is named:
 	# opensesame-extension-[extension name]
 	name='opensesame-extension-osweb',
-	version='1.1.1',
+	version='1.1.2',
 	description='OSWeb extension for OpenSesame',
 	author='Sebastiaan Mathot',
 	author_email='s.mathot@cogsci.nl',
