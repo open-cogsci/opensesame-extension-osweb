84f3029fbf3c32de2ec10e49aa322e67cfae7c02
Sebastiaan Mathot <s.mathot@cogsci.nl>
2019-09-12 20:34:31 +0200

    inline_javascript: make compatible with OpenSesame 3.3

--- opensesame_plugins/inline_javascript/inline_javascript.py
+++ opensesame_plugins/inline_javascript/inline_javascript.py
@@ -72,7 +72,9 @@ class inline_javascript(inline_script):
 class qtinline_javascript(qtinline_script):
 
 	description = _(u'Executes JavaScript code (ECMA 5.1)')
-	language = u'JavaScript'
+	language = u'JavaScript'  # For OpenSesame 3.2
+	ext = u'.js'  # For OpenSesame 3.3
+	mime_type = u'application/javascript'  # For OpenSesame 3.3
 
 	def __init__(self, name, experiment, string=None):
 
