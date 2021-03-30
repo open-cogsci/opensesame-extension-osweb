3950cccb89bd5c3a6e4e543fd3524ac4540c55e8
smathot <s.mathot@cogsci.nl>
2021-03-30 15:47:03 +0200

    Improve compatibility check

--- osweb/linter.py
+++ osweb/linter.py
@@ -17,12 +17,12 @@ You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import itertools as it
+
+
+# These items are supported but don't have a specific linter function
 SUPPORTED_ITEMS = [
     u'sequence',
-    u'loop',
-    u'sketchpad',
-    u'feedback',
-    u'mouse_response',
     u'sampler',
     u'keyboard_response',
     u'logger',
@@ -44,24 +44,40 @@ STRUCTURE_CHECK_ITEMS = [
 
 
 def check_compatibility(exp):
-
+    """Checks the compatibility of an experiment and returns all warnings as a
+    list of text strings. An empty list indicates that the experiment is
+    compatible.
+    """
     return u'\n'.join(check_supported_items(exp) + check_structure(exp))
 
 
-def check_supported_items(exp):
+def check_item(item):
+    """Checks whether an item is compatible and returns all warnings as a list
+    of text strings. An empty list indicates that the experiment is
+    compatible. If an item specific linter function is available (with the 
+    name check_item_[item type]) then this is used. Otherwise, a warning is
+    giving when the item type is not in the list of supported items.
+    """
+    try:
+        linter_fnc = eval('check_item_{}'.format(item.item_type))
+    except NameError:
+        if item.item_type not in SUPPORTED_ITEMS:
+            return [u'Item {} is not supported'.format(item.item_type)]
+        return []
+    return linter_fnc(item)
 
-    return [
-        u'Item {} is not supported'.format(item.item_type)
-        for item in exp.items.values()
-        if item.item_type not in SUPPORTED_ITEMS
-    ]
+
+def check_supported_items(exp):
+    """Checks all items and returns a list of warnings."""
+    return list(it.chain(*[check_item(item) for item in exp.items.values()]))
 
 
 def check_structure(exp):
-    
-    errors = []
-    states = {}
-    
+    """Checks the structure of the experiment for a specific issue with OSWeb
+    in which for certain items the prepare and run phases need to be called
+    in alternation, i.e. it is not allowed to call the prepare phase (or the
+    run phase) twice in a row.
+    """
     def set_state(item_name, state):
         
         if item_name not in exp.items:
@@ -84,15 +100,76 @@ def check_structure(exp):
         elif item.item_type == 'loop':
             set_state(item._item, 'prepare')
             set_state(item._item, 'run')
-        
+
+    errors = []
+    states = {}
     set_state(exp.var.start, 'prepare')
     set_state(exp.var.start, 'run')
     return errors
 
 
-# from libopensesame.experiment import experiment
-# exp = experiment(
-#     name = 'Dummy',
-#     string = '/home/sebastiaan/Downloads/Sentences-and-Letters-with-textEW_2.osexp'
-# )
-# print(check_structure(exp))
+def item_warning(item, msg):
+    return '{} ({}): {}'.format(item.name, item.item_type, msg)
+
+
+def check_item_loop(item):
+    w = []
+    if item.var.continuous != 'no':
+        w.append(item_warning(item, 'Resume after break not supported'))
+    if item.var.break_if_on_first != 'yes':
+        w.append(
+            item_warning(
+                item,
+                'Disabling evaluate on first cycle not supported'
+            )
+        )
+    if item.var.source != 'table':
+        w.append(item_warning(item, 'File source not supported'))
+    if item._constraints:
+        w.append(item_warning(item, 'Constraints not supported'))
+    return w
+
+
+def check_item_sketchpad(item):
+    w = []
+    for e in item.elements:
+        if e.properties.get('rotation', 0) != 0:
+            w.append(item_warning(item, 'Rotation not supported'))
+        if e.properties.get('color', '').lower()[:3] in ('hsv', 'hsl', 'lab'):
+            w.append(item_warning(
+                item, 'hsv, hsl, and lab colorspaces not supported')
+            )
+    return w
+
+
+def check_item_feedback(item):
+    return check_item_sketchpad(item)
+    
+    
+def check_item_mouse_response(item):
+    w = []
+    if item.var.linked_sketchpad:
+        w.append(item_warning(item, 'Linked sketchpad not supported'))
+    if item.var.event_type == 'mouserelease':
+        w.append(item_warning(item, 'Mouse release not supported'))
+    return w
+
+
+def check_item_keyboard_response(item):
+    w = []
+    if item.var.event_type == 'keyrelease':
+        w.append(item_warning(item, 'Key release not supported'))
+    return w
+
+
+def check_item_sampler(item):
+    w = []
+    if item.var.pitch != 1:
+        w.append(item_warning(item, 'Changing pitch not supported'))
+    if item.var.pan != 0:
+        w.append(item_warning(item, 'Panning not supported'))
+    if item.var.stop_after != 0:
+        w.append(item_warning(item, 'Stop after not supported'))
+    if item.var.fade_in != 0:
+        w.append(item_warning(item, 'Fade in not supported'))
+    return w
