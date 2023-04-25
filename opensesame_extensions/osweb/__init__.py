453a5e3f643e73ca542e6748d3995e647e7312b1
smathot <s.mathot@cogsci.nl>
2023-04-25 17:58:18 +0200

    Bump to 2.0.0.0a16

--- opensesame_extensions/osweb/__init__.py
+++ opensesame_extensions/osweb/__init__.py
@@ -1,5 +1,5 @@
 """Extension to integrate OSWeb into OpenSesame"""
 
-__version__ = '2.0.0.0a15'
+__version__ = '2.0.0.0a16'
 # The name of the package to check for updates on conda and pip
 packages = ['opensesame-extension-osweb']
