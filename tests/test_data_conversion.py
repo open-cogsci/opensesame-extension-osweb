e8bcebb8c5947417e15f25ccef15108d0fab77ca
smathot <s.mathot@cogsci.nl>
2023-05-01 21:40:22 +0200

    Add unit tests for data conversion

---
+++ tests/test_data_conversion.py
@@ -0,0 +1,43 @@
+"""
+This file is part of OpenSesame.
+
+OpenSesame is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenSesame is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import unittest
+from libopensesame.py3compat import *
+from opensesame_extensions.osweb.oswebext.osweb import data
+
+
+class DataConversionTestCase(unittest.TestCase):
+    
+    def test_txt(self):
+        dm = data.parse_results('tests/data/test-data.txt')
+        assert len(dm) > 0 and dm.columns
+        
+    def test_json(self):
+        dm = data.parse_results('tests/data/test-data.json')
+        assert len(dm) > 0 and dm.columns
+        
+    def test_jrzip(self):
+        dm = data.parse_results('tests/data/test-data.jrzip')
+        assert len(dm) > 0 and dm.columns
+        
+    def test_zip(self):
+        dm = data.parse_results('tests/data/test-data.zip')
+        assert len(dm) > 0 and dm.columns
+
+        
+if __name__ == '__main__':
+    
+    unittest.main()
