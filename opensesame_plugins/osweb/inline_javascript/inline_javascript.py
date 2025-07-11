bbf5cfc6e7eea412a5e2b4a7e5651ac76c5b54fd
smathot <s.mathot@cogsci.nl>
2025-07-11 10:23:18 +0200

    Make compatible with OpenSesme 4.1 - Remove PyQode and desktop runtime (which was already unused)

--- opensesame_plugins/osweb/inline_javascript/inline_javascript.py
+++ opensesame_plugins/osweb/inline_javascript/inline_javascript.py
@@ -1,100 +1,28 @@
 # coding=utf-8
 from libopensesame.py3compat import *
-import js2py
-from js2py.translators import translate_js
 import esprima
 from esprima.nodes import FunctionDeclaration, VariableDeclaration, \
-    ExpressionStatement, AssignmentExpression, StaticMemberExpression, \
-    BlockStatement
+    ExpressionStatement, AssignmentExpression, StaticMemberExpression
 from libopensesame.inline_script import InlineScript
 from libqtopensesame.items.inline_script import InlineScript as QtInlineScript
-from libqtopensesame.items.qtplugin import QtPlugin
-from pyqode.core.widgets import SplittableCodeEditTabWidget
 from libqtopensesame.misc.translate import translation_context
+from libqtopensesame.items.qtplugin import QtPlugin
 from libopensesame.oslogging import oslogger
-from . import javascript_workspace_api as api
-_ = translation_context(u'inline_javascript', category=u'plugin')
-
-
-class JavaScriptWorkspace(js2py.EvalJs):
-
-    def __init__(self, experiment):
-
-        self.__dict__[u'experiment'] = experiment
-        self.__dict__[u'_globals'] = {}
-        self.__dict__[u'_persistent'] = {}
-        self.init_globals()
-        js2py.EvalJs.__init__(self, self._globals)
-
-    def init_globals(self):
-
-        """
-        desc:
-            Initialize the global workspace.
-        """
-        # By setting the __name__ global, the workspace will operate as a
-        # module, so that e.g. import statements don't suffer from locality.
-        self._globals.update({
-            u'__name__': u'javascript_workspace',
-            u'vars': self.experiment.var,
-            u'persistent': self._persistent
-        })
-        for name, obj in api.__dict__.items():
-            if name.startswith('_'):
-                continue
-            self._globals[name] = obj
-
-    def _compile(self, js=None, use_compilation_plan=False):
-
-        code = translate_js(
-            js,
-            '',
-            use_compilation_plan=use_compilation_plan
-        )
-        return compile(
-            code,
-            '<EvalJS snippet>',
-            'exec'
-        )
-
-    def _exec(self, compiled):
-
-        exec(compiled, self._context)
+_ = translation_context('inline_javascript', category='plugin')
 
 
 class InlineJavascript(InlineScript):
-
-    def reset(self):
-
-        self.var._prepare = u''
-        self.var._run = u''
-
-    @property
-    def workspace(self):
-
-        if not hasattr(self.experiment, u'javascript_workspace'):
-            self.experiment.javascript_workspace = JavaScriptWorkspace(
-                self.experiment)
-        return self.experiment.javascript_workspace
+    pass  # There is no desktop runtime for OSWeb
 
 
 class QtInlineJavascript(QtInlineScript):
 
-    language = u'JavaScript'  # For OpenSesame 3.2
-    ext = u'.js'  # For OpenSesame 3.3
-    mime_type = u'application/javascript'  # For OpenSesame 3.3
-
+    language = 'javascript'
+    
     def __init__(self, name, experiment, string=None):
-
-        # This requires pyqode_extras, which is not available in opensesamerun
-        # and therefore we import it only here.
-        from .javascript_code_edit import JavaScriptCodeEdit
-
         self._var_cache = None
-        if self.mime_type not in SplittableCodeEditTabWidget.editors:
-            SplittableCodeEditTabWidget.register_code_edit(JavaScriptCodeEdit)
         InlineScript.__init__(self, name, experiment, string)
-        QtPlugin.__init__(self, plugin_file=__file__)
+        QtPlugin.__init__(self, plugin_file=__file__)    
 
     def item_icon(self):
 
@@ -140,4 +68,3 @@ class QtInlineJavascript(QtInlineScript):
         except Exception as e:
             oslogger.debug(f'failed to extract assignments: {e}')
             return []
-    
