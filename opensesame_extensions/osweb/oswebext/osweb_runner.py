9ab133d2300949518e2b14eb89dce13e601ebcd9
Sebastiaan Mathot <s.mathot@cogsci.nl>
2025-07-15 07:31:35 +0200

    Update runner for OpenSesame 4.1

--- opensesame_extensions/osweb/oswebext/osweb_runner.py
+++ opensesame_extensions/osweb/oswebext/osweb_runner.py
@@ -16,8 +16,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with OpenSesame.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-from libopensesame.py3compat import *
 from libqtopensesame.runners.base_runner import BaseRunner
 
 
@@ -31,14 +29,24 @@ class OSWebRunner(BaseRunner):
         return return_values.get('Oswebext', None)
     
     def init_experiment(self, quick=False, fullscreen=False):
-        # Get and set the subject number
-        subject_nr = self.get_subject_nr(quick=quick)
-        if subject_nr is None:
-            return False
-        # Get and set the logfile
-        logfile = self.get_logfile(quick=quick, subject_nr=subject_nr,
-                                   file_extension='.json')
-        if logfile is None:
+        # Get and set the subject number and log file. In OpenSesame 4.1 this
+        # has been unified into a single dialog
+        try:
+            # OpenSesame <= 4.0
+            subject_nr = self.get_subject_nr(quick=quick)
+            logfile = self.get_logfile(quick=quick, subject_nr=subject_nr,
+                                       file_extension='.json')
+        except AttributeError:
+            # OpenSesame >= 4.1
+            settings = self.get_experiment_settings(quick=quick)
+            if settings is None:
+                subject_nr = None
+                logfile = None
+            else:
+                subject_nr = settings['subject_nr']
+                logfile = settings['logfile']
+                fullscreen = settings['fullscreen']
+        if subject_nr is None or logfile is None:
             return False
         self._subject_nr = subject_nr
         self._logfile = logfile
